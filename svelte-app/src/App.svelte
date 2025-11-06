a66a5ff7fd3dfd2152e3be8279073267f4d14de8
Jeshua Yam <jeshuayam3024@gmail.com>
2025-11-06 13:07:27 -0600

    svelte

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Hijo from './Hijo.svelte';
+	export let name;
+	let nombre;
+
+	let contador = 0;
+	function incrementar(){
+		contador = contador + 1;
+	}
+
+	function saludar(){
+		alert(nombre ? nombre : "nulo");
+	}
+</script>
+
+<main>
+	<p>Ola {name}!</p>
+	<!--<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>-->
+	<h1>Contar: {contador}</h1>
+	<button on:click={incrementar}>Contar++</button>
+	<Hijo texto="Guardar" color="red"></Hijo>
+	<Hijo texto="Josear"></Hijo>
+	<input bind:value={nombre} type="text" placeholder="Tu nombre w">
+	<button on:click={saludar}>SAludameE</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ svelte-app/src/Hijo.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let texto = "Botón";
+    export let color = 'blue';
+</script>
+
+<!--Markup html-->
+<button style="background-color: {color};">
+    {texto}
+</button>
+
+<style>
+    button{
+        color: white;
+    }
+</style>
